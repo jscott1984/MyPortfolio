847d08cfe9b7fb85cfacd9d1e14cac5382ce5fb8
Joseph Scott <joseph746567@hotmail.com>
2021-05-07 01:37:46 -0600

    I added a class. I also put in the input to output to the user and get the value from the user. the last thing I did was a made a array of int that start at 2 and then count up by 1 to the vlue that use enter in.

--- PythonPrime/PythonPrime.py
+++ PythonPrime/PythonPrime.py
@@ -1,2 +1,28 @@
+from array import *
 
+class Node:
+    def __init__(data = int):
+        Node.data = int
+        Node.Head = Node
+        Node.Tail = Node
+
+List = Node()
+List.Head = List
+List.Tail = List
+
+count = 2
+value = input("Find all primes below what number? ")
+
+numbers = array('l', [count])
+
+for x in range(int(value) - 2):
+    count = count + 1
+    numbers.append(count)
+
+for x in range(int(value) - 2):
+    List.data = numbers[x]
+    print(" list of Numbers ", List.data)
+
+for x in numbers:
+    print(" list of Numbers ", x)
 
